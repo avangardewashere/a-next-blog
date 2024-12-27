a2e58e0b797b68cf2a4e10e2cd2bf907cb230998
avangardewashere <avelpanaligan@gmail.com>
2024-12-28 03:13:55 +0800

    add/components-sturtures-and-templates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,5 @@
-import Navbar from "@/components/navbar/navbar";
 import styles from "./homepage.module.css";
-import Footer from "@/components/footer/footer";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Navbar />
-      <Footer />
-    </div>
-  );
+  return <div className={styles.container}></div>;
 }
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./index.module.css";
+
+const Footer = () => {
+  return <div className={styles.container}>Menu</div>;
+};
+
+export default Footer;
