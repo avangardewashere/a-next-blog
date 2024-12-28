0e14040445ecd5d32e31670ac789c36b735be04e
avangardewashere <avelpanaligan@gmail.com>
2024-12-28 15:39:23 +0800

    started/navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import styles from "./index.module.css";
+import Image from "next/image";
 
 const Navbar = () => {
-  return <div className={styles.container}>Navbar
- 
-  </div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src="/facebook.png" alt="social-icon" width={24} height={24} />
+      </div>
+      <div className={styles.logo}>Avant Blog</div>
+      <div className={styles.links}></div>
+    </div>
+  );
 };
 
 export default Navbar;
