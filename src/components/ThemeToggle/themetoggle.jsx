472d3da9d1ed6bce851bfdb08aad0e89461bf96c
avangardewashere <avelpanaligan@gmail.com>
2024-12-28 17:55:37 +0800

    add/theme-color-toggle-functionality

--- src/components/ThemeToggle/themetoggle.jsx
+++ src/components/ThemeToggle/themetoggle.jsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 import { ThemeContext } from "@/context/ThemeContext";
 
 const ThemeToggle = () => {
-  const { theme } = useContext(ThemeContext);
-  console.log("test", theme);
+  const { theme,toggle } = useContext(ThemeContext);
+//   console.log("test", theme);
   return (
-    <div className={styles.container}>
+    <div onClick={toggle} className={styles.container}>
       <Image src="/moon.png" alt="" width={14} height={14} />
       <div className={styles.ball}></div>
       <Image src="/sun.png" alt="" width={14} height={14} />
