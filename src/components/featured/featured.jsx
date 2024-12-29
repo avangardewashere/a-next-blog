e0e53d961af0feca43e003b606d67ca235597294
avangardewashere <avelpanaligan@gmail.com>
2024-12-29 12:41:50 +0800

    add/featured-post-structure

--- src/components/featured/featured.jsx
+++ src/components/featured/featured.jsx
@@ -2,7 +2,27 @@ import React from "react";
 import styles from "./index.module.css";
 
 const Featured = () => {
-  return <div className={styles.container}>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Avant Blog </b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores
+            labore similique suscipit perspiciatis dolorum eveniet accusamus
+            mollitia corrupti blanditiis nemo.
+          </p>
+          <button>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
