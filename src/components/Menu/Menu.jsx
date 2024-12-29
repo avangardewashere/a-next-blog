1cc2c9f07694c27e1bba507e97c73b1b6a440f00
avangardewashere <avelpanaligan@gmail.com>
2024-12-29 23:51:40 +0800

    addsidebar-item-stucture

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import styles from "./index.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Menu = () => {
-  return <div className={styles.container}>Menu</div>;
+  return (
+    <div className={styles.container}>
+      <h2>{"What's hot"}</h2>
+      <h1>Most Popular</h1>
+      <div className={styles.items}>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>
+              Travel
+            </span>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+            <div className={styles.detail}>
+              <span className={styles.username }>John Doe</span>
+              <span className={styles.date}>10.03.2023</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
