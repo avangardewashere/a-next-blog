761e72056830a1fe510253fc043fc101f5e14be6
avangardewashere <avelpanaligan@gmail.com>
2024-12-29 23:26:52 +0800

    add/cardList-component-structure

--- src/components/cardList/cardList.jsx
+++ src/components/cardList/cardList.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import styles from "./index.module.css";
 import Pagination from "../Pagination/pagination";
+import Image from "next/image";
 
 const CardList = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.title}>Recent Posts</div>
+      <div className={styles.posts}>
+        <div className={styles.post}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" fill alt="" />
+          </div>
+          <div className={styles.textContainer}></div>
+        </div>
+      </div>
       <Pagination />
     </div>
   );
