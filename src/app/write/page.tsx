2eb473e1a373b7a7075a98c230eaeb981ec9428e
avangardewashere <avelpanaligan@gmail.com>
2025-01-01 08:35:03 +0800

    add/write-component-structure

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./index.module.css";
 
 const Write = () => {
-  return <div className={styles.container}>Write</div>;
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(false);
+  return (
+    <div className={styles.container}>
+      <input type="text" placeholder="Title" />
+
+      <div className={styles.editor}>
+        <button className={styles.button}>➕</button>
+        {open && (
+          <>
+            <div className={styles.add}>
+              <button className={styles.addButton}>
+                <span> 🙂</span>
+              </button>
+              <button className={styles.addButton}>
+                <span> 🚀</span>
+              </button>
+              <button className={styles.addButton}>
+                <span> 📽️</span>
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Write;
